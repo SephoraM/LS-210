4a19b97ed96c49662c4c419aa21752462569a5dd
Sephora <seph156@gmail.com>
2019-03-11 22:32:37 +0100

    removed comments from rot13

--- logic-flow/rot13.js
+++ logic-flow/rot13.js
@@ -1,27 +1,9 @@
-/*
-Rot13 ("rotate by 13 places") is a letter-substitution cipher that translates
-a String into a new String:
-
-For each character in the original String:
-
-If the character is a letter in the modern English alphabet, change it to the
-character 13 positions later in the alphabet. So a becomes n. If you reach the
-end of the alphabet, return to the beginning. So o becomes b.
-
-Letter transformations preserve case. A becomes N while a becomes n.
-
-Don't modify characters that are not letters.
-
-Write a Function, rot13, that takes a String and returns that String transformed
-by the rot13 cipher.
-*/
-
 function isUpper(char) {
-  return !!(char >= 'A' && char <= 'Z');
+  return char >= 'A' && char <= 'Z';
 }
 
 function isLower(char) {
-  return !!(char >= 'a' && char <= 'z');
+  return char >= 'a' && char <= 'z';
 }
 
 function isAlphabetic(char) {
@@ -29,7 +11,7 @@ function isAlphabetic(char) {
 }
 
 function isFirst13(char) {
-  return !!((isUpper(char) && char < 'N') || (isLower(char) && char < 'n'));
+  return (isUpper(char) && char < 'N') || (isLower(char) && char < 'n');
 }
 
 function rot13(string) {
