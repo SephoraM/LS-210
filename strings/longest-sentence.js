513cc97379e1b0ce308db90335e96efd90ef4258
Sephora <seph156@gmail.com>
2019-04-02 15:30:26 +0200

    longest string exercise edit accounting for extra spaces

--- strings/longest-sentence.js
+++ strings/longest-sentence.js
@@ -6,7 +6,7 @@ const keepLongest = (last, current) => (last.length > current.length ? last : cu
 const longestSentence = (text) => {
   const longest = text
     .match(sentenceRegex)
-    .map(sentence => sentence.trim().split(' '))
+    .map(sentence => sentence.trim().split(/\s+/))
     .reduce(keepLongest, []);
 
   console.log(`${longest.join(' ')}\n`);
@@ -19,7 +19,7 @@ const longText = 'Four score and seven years ago our fathers brought forth'
   + ' dedicated to the proposition that all men are created'
   + ' equal.'
   + ' Now we are engaged in a great civil war, testing whether'
-  + ' that nation, or any nation so conceived and so dedicated,'
+  + ' that nation, or any nation so conceived and so     dedicated,'
   + ' can long endure. We are met on a great battlefield of that'
   + ' war. We have come to dedicate a portion of that field, as'
   + ' a final resting place for those who here gave their lives'
